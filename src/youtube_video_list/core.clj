3523061cbe01832e445d5c1243f03479894e89a6
Rohit Patnaik <quanticle@quanticle.net>
2023-01-05 02:55:15 -0600

    Started work on getting all videos from a channel
    - Got the code to retrieve the uploads playlist for the channel
    working
    - Started work on code to get all the videos in the upload playlist

--- src/youtube_video_list/core.clj
+++ src/youtube_video_list/core.clj
@@ -1,5 +1,60 @@
 (ns youtube-video-list.core
+  (:require [clojure.java.io :as io]
+            [clojure.string :as str]
+            [clj-http.client :as http-client]
+            [clojure.data.json :as json])
   (:gen-class))
+
+(def client-key-file-name "client-key")
+
+(defn load-client-key [file-name]
+  "Loads the client key from the given file. The file is assumed to have just 
+   the API key, with a possible trailing newline."
+  (str/trim (slurp (io/resource file-name))))
+
+(defn get-uploads-playlist-id [api-key channel-url]
+  "Get the ID of the playlist representing the full list of uploaded video for 
+   the given channel. The channel URL can either be one that contains a user ID
+   (i.e. https://www.youtube.com/user/nismotv2013/videos) or, one that contains
+   the channel ID (i.e. 
+   https://www.youtube.com/channel/UCy0tKL1T7wFoYcxCe0xjN6Q/videos)"
+  (let [channel-id (re-find #"/channel/([^/]+)/?" channel-url)
+        user-id (re-find #"/user/([^/]+)/?" channel-url)
+        query-params (cond
+                       channel-id {:key api-key
+                                   :id (channel-id 1)
+                                   :part "contentDetails"}
+                       user-id {:key api-key
+                                :forUsername (user-id 1)
+                                :part "contentDetails"}
+                       :else (throw (Exception. 
+                                     (str "Could not determine either user ID or channel ID from " channel-url))))
+        http-response (http-client/get
+                       "https://www.googleapis.com/youtube/v3/channels"
+                       {:accept :json
+                        :query-params query-params})]
+    (if (= (:status http-response) 200)
+      (-> http-response
+          :body
+          (json/read-str :key-fn keyword)
+          :items
+          (get 0)
+          :contentDetails
+          :relatedPlaylists
+          :uploads)
+      (throw (Exception. (str "Did not get a valid HTTP response from the YouTube API: " (:status http-response)))))))
+
+(defn get-video-data [playlist-items-response]
+  "Extracts the fields we're interested in (video title, upload date and video
+   ID) from the PlaylistItems API response"
+  (map #(hash-map :video-title (get-in % [:snippet :title])
+          :video-id (get-in % [:contentDetails :videoId])
+          :upload-date (get-in % [:contentDetails :videoPublishedAt]))
+       (:items playlist-items-response)))
+
+
+          
+      
 
 (defn -main
   "I don't do a whole lot ... yet."
--- test/youtube_video_list/core_test.clj
+++ test/youtube_video_list/core_test.clj
@@ -1,7 +1,61 @@
 (ns youtube-video-list.core-test
   (:require [clojure.test :refer :all]
+            [clojure.java.io :as io]
+            [mockery.core :refer :all]
             [youtube-video-list.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest test-get-uploads-playlist-id
+  (testing "Get uploads playlist for username"
+    (with-mock mock-http
+      {:target :clj-http.client/get
+       :return {:body (slurp (io/resource "channels_forUsername_output.txt"))
+                :status 200}}
+      (is (= (get-uploads-playlist-id "test-api-key" "https://www.youtube.com/user/nismotv2013/videos") "UUaTxfj0BzL-MaCy-YUqPRoQ"))
+      (is (= (:call-args @mock-http)
+             ["https://www.googleapis.com/youtube/v3/channels"
+              {:accept :json
+               :query-params {
+                              :key "test-api-key"
+                              :forUsername "nismotv2013"
+                              :part "contentDetails"}}]))))
+  (testing "Get uploads playlist for channel ID"
+    (with-mock mock-http
+      {:target :clj-http.client/get
+       :return {:body (slurp (io/resource "channels_channelId_output.txt"))
+                :status 200}}
+      (is (= (get-uploads-playlist-id "test-api-key" "https://www.youtube.com/channel/UCoSrY_IQQVpmIRZ9Xf-y93g/videos") "UUoSrY_IQQVpmIRZ9Xf-y93g"))
+      (is (= (:call-args @mock-http)
+             ["https://www.googleapis.com/youtube/v3/channels"
+              {:accept :json
+               :query-params {
+                              :key "test-api-key"
+                              :id "UCoSrY_IQQVpmIRZ9Xf-y93g"
+                              :part "contentDetails"}}]))))
+  (testing "Invalid URL"
+    (is (thrown-with-msg? Exception #"Could not determine" (get-uploads-playlist-id "test-api-key" "https://www.google.com")))))
+
+
+(deftest test-get-video-data
+  (testing "No videos"
+    (is (= (get-video-data {:items []}) [])))
+  (testing "One video"
+    (is (= (get-video-data {:items [{:snippet {:title "Test video"}
+                                     :contentDetails {:videoId "test_id"
+                                                      :videoPublishedAt "test date"}}]})
+           '({:video-title "Test video"
+              :video-id "test_id"
+              :upload-date "test date"}))))
+  (testing "Multiple videos"
+    (is (= (get-video-data {:items
+                            [{:snippet {:title "Test video 1"}
+                              :contentDetails {:videoId "test_id_1"
+                                               :videoPublishedAt "test date 1"}}
+                             {:snippet {:title "Test video 2"}
+                              :contentDetails {:videoId "test_id_2"
+                                               :videoPublishedAt "test date 2"}}]})
+           '({:video-title "Test video 1"
+              :video-id "test_id_1"
+              :upload-date "test date 1"}
+             {:video-title "Test video 2"
+              :video-id "test_id_2"
+              :upload-date "test date 2"})))))
